bce3efc8baeb0599bd679e1332008752692add5a
ruttapol <ruttapolkosol@gmail.com>
2021-09-22 19:07:19 +0700

    first

--- app.py
+++ app.py
@@ -754,6 +754,312 @@ def sell_search():
     return render_template('sell.html', search_result=result, shop_array=shop_array)
 
 
+@app.route('/sell_by_month', methods=['POST'])
+def sell_by_month():
+    # int_features = [int(x) for x in request.form.values()]
+    # final_features = [np.array(int_features)]
+    # prediction = model.predict(final_features)
+    # print(int_features[0])
+    import os
+    file_name = './pic.png'
+
+    if not firebase_admin._apps:
+        cred = credentials.Certificate('firebase-sdk.json')
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://microbittempreader-16e2d-default-rtdb.firebaseio.com/'
+        })
+
+    ref = db.reference('Shop')
+    stateSearch = request.form.get("stateSearch")
+
+    if stateSearch == "ALL":
+        snapshot = ref.order_by_key().limit_to_last(11).get()
+    else:
+        snapshot = ref.order_by_child('state').equal_to(stateSearch).limit_to_last(4).get()
+
+    # snapshot = ref.order_by_child('time').start_at('20210919').get()
+    statel = []
+
+    share = []
+    state_array = []
+    price_array = []
+    shop_array = []
+    shop_list = ""
+
+    inttemp = 0
+
+    for user, val in snapshot.items():
+
+        state_list = ""
+        shop_list = ""
+
+        email = val.get("email")
+        email = email.strip()
+
+        if email is None:
+            email = "-"
+
+        if email == "":
+            email = "-"
+
+        phone = val.get("phone")
+        contactPerson = val.get("contactPerson")
+        state = val.get("state")
+        city = val.get("city")
+        area = val.get("area")
+        price = val.get("price")
+        textArea = val.get("textArea")
+        textArea = textArea.replace(",", " ")
+        time = val.get("time")
+        # print(phone)
+        # print(state)
+        # print(area)
+        # print(price)
+        # print(time)
+
+        if stateSearch == "ALL":
+            state_array.append(str(state))
+            price_array.append(int(price))
+            shop_list = shop_list + email + "," + phone + "," + contactPerson + " ," + state + "," + city + "," + area + " m2 , " + price + " dollars, " + textArea + "," + time
+            shop_array.append(shop_list)
+
+            if statel.__contains__(state) == False:
+                inttemp = 0
+                pricesnapshot = ref.order_by_child('state').equal_to(state).get()
+                for user1, val1 in pricesnapshot.items():
+                    inttemp = inttemp + int(val1.get('price'))
+                # print('  '+str(inttemp))
+                share.append(inttemp / 100)
+                statel.append(state)
+            else:
+                inttemp = 0
+                # share.append(inttemp)
+            result = "Found record"
+        else:
+            if stateSearch == state:
+                state_array.append(str(state))
+                price_array.append(int(price))
+                shop_list = shop_list + email + "," + phone + "," + contactPerson + " ," + state + "," + city + "," + area + " m2 , " + price + " dollars, " + textArea + "," + time
+                shop_array.append(shop_list)
+                result = "Found record"
+            else:
+                result = "No record found"
+
+    # print(len(share))
+    # print(statel)
+    # print(state_array)
+    # print(price_array)
+    # print(shop_array)
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # create data
+
+    x = np.arange(len(state_array))
+    # y1 = [3400, 5600, 1200]
+    y1 = price_array
+    y2 = [12, 56, 78, 45, 90]
+    width = 0.40
+
+    # plot data in grouped manner of bar type
+    plt.bar(x - 0.2, y1, width)
+    # plt.bar(x+0.2, y2, width)
+    state = ['A', 'B', ' C', 'D', 'E']
+
+    # state = state_array
+    # state = ['MA', 'WA', 'AR' ,'MA','WA','WA']
+
+    # print(x)
+    # print(state_array)
+    plt.xticks(x, state_array)
+    plt.xlabel("State")
+    plt.ylabel("Amount")
+    plt.legend(["Shop"])
+
+    import matplotlib.pyplot as plt
+
+    plt.savefig('static/shop_bar' + '.png')
+    plt.close()
+
+    # The slice names of a population distribution pie chart
+    # pieLabels = 'Asia', 'Africa', 'Europe', 'North America', 'South America', 'Australia'
+    pieLabels = statel
+    # Population data
+    # populationShare = [11,48.69, 16, 9.94, 7.79, 1,4.68]
+
+    # print(share)
+    populationShare = share
+    figureObject, axesObject = plt.subplots()
+
+    # Draw the pie chart
+    axesObject.pie(populationShare,
+                   labels=pieLabels,
+                   autopct='%1.2f',
+                   startangle=90)
+
+    # Aspect ratio - equal means pie is a circle
+    axesObject.axis('equal')
+
+    plt.savefig('static/shop_pie' + '.png')
+    plt.close()
+
+    return render_template('sell.html', search_result=result, shop_array=shop_array)
+
+
+@app.route('/sell_by_brand', methods=['POST'])
+def sell_by_brand():
+    # int_features = [int(x) for x in request.form.values()]
+    # final_features = [np.array(int_features)]
+    # prediction = model.predict(final_features)
+    # print(int_features[0])
+    import os
+    file_name = './pic.png'
+
+    if not firebase_admin._apps:
+        cred = credentials.Certificate('firebase-sdk.json')
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://microbittempreader-16e2d-default-rtdb.firebaseio.com/'
+        })
+
+    ref = db.reference('Shop')
+    stateSearch = request.form.get("stateSearch")
+
+    if stateSearch == "ALL":
+        snapshot = ref.order_by_key().limit_to_last(11).get()
+    else:
+        snapshot = ref.order_by_child('state').equal_to(stateSearch).limit_to_last(4).get()
+
+    # snapshot = ref.order_by_child('time').start_at('20210919').get()
+    statel = []
+
+    share = []
+    state_array = []
+    price_array = []
+    shop_array = []
+    shop_list = ""
+
+    inttemp = 0
+
+    for user, val in snapshot.items():
+
+        state_list = ""
+        shop_list = ""
+
+        email = val.get("email")
+        email = email.strip()
+
+        if email is None:
+            email = "-"
+
+        if email == "":
+            email = "-"
+
+        phone = val.get("phone")
+        contactPerson = val.get("contactPerson")
+        state = val.get("state")
+        city = val.get("city")
+        area = val.get("area")
+        price = val.get("price")
+        textArea = val.get("textArea")
+        textArea = textArea.replace(",", " ")
+        time = val.get("time")
+        # print(phone)
+        # print(state)
+        # print(area)
+        # print(price)
+        # print(time)
+
+        if stateSearch == "ALL":
+            state_array.append(str(state))
+            price_array.append(int(price))
+            shop_list = shop_list + email + "," + phone + "," + contactPerson + " ," + state + "," + city + "," + area + " m2 , " + price + " dollars, " + textArea + "," + time
+            shop_array.append(shop_list)
+
+            if statel.__contains__(state) == False:
+                inttemp = 0
+                pricesnapshot = ref.order_by_child('state').equal_to(state).get()
+                for user1, val1 in pricesnapshot.items():
+                    inttemp = inttemp + int(val1.get('price'))
+                # print('  '+str(inttemp))
+                share.append(inttemp / 100)
+                statel.append(state)
+            else:
+                inttemp = 0
+                # share.append(inttemp)
+            result = "Found record"
+        else:
+            if stateSearch == state:
+                state_array.append(str(state))
+                price_array.append(int(price))
+                shop_list = shop_list + email + "," + phone + "," + contactPerson + " ," + state + "," + city + "," + area + " m2 , " + price + " dollars, " + textArea + "," + time
+                shop_array.append(shop_list)
+                result = "Found record"
+            else:
+                result = "No record found"
+
+    # print(len(share))
+    # print(statel)
+    # print(state_array)
+    # print(price_array)
+    # print(shop_array)
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # create data
+
+    x = np.arange(len(state_array))
+    # y1 = [3400, 5600, 1200]
+    y1 = price_array
+    y2 = [12, 56, 78, 45, 90]
+    width = 0.40
+
+    # plot data in grouped manner of bar type
+    plt.bar(x - 0.2, y1, width)
+    # plt.bar(x+0.2, y2, width)
+    state = ['A', 'B', ' C', 'D', 'E']
+
+    # state = state_array
+    # state = ['MA', 'WA', 'AR' ,'MA','WA','WA']
+
+    # print(x)
+    # print(state_array)
+    plt.xticks(x, state_array)
+    plt.xlabel("State")
+    plt.ylabel("Amount")
+    plt.legend(["Shop"])
+
+    import matplotlib.pyplot as plt
+
+    plt.savefig('static/shop_bar' + '.png')
+    plt.close()
+
+    # The slice names of a population distribution pie chart
+    # pieLabels = 'Asia', 'Africa', 'Europe', 'North America', 'South America', 'Australia'
+    pieLabels = statel
+    # Population data
+    # populationShare = [11,48.69, 16, 9.94, 7.79, 1,4.68]
+
+    # print(share)
+    populationShare = share
+    figureObject, axesObject = plt.subplots()
+
+    # Draw the pie chart
+    axesObject.pie(populationShare,
+                   labels=pieLabels,
+                   autopct='%1.2f',
+                   startangle=90)
+
+    # Aspect ratio - equal means pie is a circle
+    axesObject.axis('equal')
+
+    plt.savefig('static/shop_pie' + '.png')
+    plt.close()
+
+    return render_template('sell.html', search_result=result, shop_array=shop_array)
+
+
 @app.after_request
 def add_header(response):
     response.cache_control.max_age = 0
